6bb14df608d225820140a9c1f1e5e3918bc3043d
kanokpich18 <s6603052421186@email.kmutnb.ac.th>
2024-02-21 15:42:53 +0700

    Update webboard using bootstrap
    Update file index.php login.php vedify.php
    new file nav.php (Work 7)

--- verify.php
+++ verify.php
@@ -13,8 +13,7 @@
     <title>Verify</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard GungGing</h1>
-    <hr>
+    
     <div style="text-align: center;">
     <?php 
      $login=$_POST['login'];
@@ -23,19 +22,24 @@
         $_SESSION["username"] = "admin";
         $_SESSION["role"] = "a";
         $_SESSION["id"] = session_id();
-        echo"ยินดีต้อนรับคุณ ADMIN";
+        header("location:index.php");
+        die();
+       // echo"ยินดีต้อนรับคุณ ADMIN";
     }elseif($login=="member" && $pwd=="mem1234"){
         $_SESSION["username"] = "member";
         $_SESSION["role"] = "m"; 
         $_SESSION["id"] = session_id();
-        echo "ยินดีต้อนรับคุณ MEMBER";
+        header("location:index.php");
+        die();
+       // echo "ยินดีต้อนรับคุณ MEMBER";
     }else{
-        echo"ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
+        $_SESSION['error']='error';
+        header("location:login.php");
+        die();
+       // echo"ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
     }
     ?>
-    <br>
-    <a tyle="text-align: center;"><a href="index.php">กลับไปหน้าหลัก</a></a> 
-</a>
+    
 
 </body>
 </html>
